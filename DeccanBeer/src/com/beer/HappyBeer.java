c91c33750e701828e053394a9d6ca77f2110ccba
ittab <ittab@LAPTOP-LPEVDRT9>
2021-07-31 10:54:12 +0530

    g

---
+++ DeccanBeer/src/com/beer/HappyBeer.java
@@ -0,0 +1,10 @@
+package com.beer;
+
+public class HappyBeer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Happy Beers Day !! Cheers !!!");
+	}
+
+}
